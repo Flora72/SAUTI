43630938356d00d68a22ede1d31d83651f3c0d41
Flora72 <zahratheo569@gmail.com>
2025-11-04 17:32:11 +0300

    chore: Wire Home and StoryForm components into App routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,8 @@
-import StoryForm from './StoryForm.jsx'
-import StoryList from './StoryList.jsx'
+// src/App.jsx
+import Home from './pages/Home'
 
 function App() {
-  return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold text-center mb-6">Sauti Story Portal</h1>
-      <StoryForm />
-      <StoryList />
-    </div>
-  )
+  return <Home />
 }
 
 export default App
