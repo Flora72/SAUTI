63453b6f283deaabbb36e02d2e2c41cb00e89f0b
Flora72 <zahratheo569@gmail.com>
2025-11-06 21:57:30 +0300

    Set up React Router and updated dependencies for page navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
-// src/App.jsx
-import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
 
 function App() {
-  return <Home />
+  return (
+    <div className="min-h-screen bg-base-100 text-base-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
