3ddc01418d1ba6012ac980612475492ba64566c4
Flora72 <zahratheo569@gmail.com>
2025-11-12 13:15:34 +0300

    Refactor App layout and routing structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
-// src/App.jsx
-import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import ShareStory from './pages/ShareStory.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
 
 function App() {
-  return <Home />
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/share" element={<ShareStory />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  )
 }
 
 export default App
