29cbe1397babe9d33a5a543c92c98c6bd3d9e981
Flora72 <zahratheo569@gmail.com>
2025-11-12 13:16:30 +0300

    Add responsive Navbar with SAUTI branding and navigation links

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100 shadow-md px-6">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl">SAUTI</Link>
+      </div>
+      <div className="flex-none space-x-2">
+        <Link to="/" className="btn btn-sm btn-outline">Home</Link>
+        <Link to="/share" className="btn btn-sm btn-outline">Share Story</Link>
+        <Link to="/about" className="btn btn-sm btn-outline">About</Link>
+        <Link to="/contact" className="btn btn-sm btn-outline">Contact</Link>
+      </div>
+    </div>
+  )
+}
