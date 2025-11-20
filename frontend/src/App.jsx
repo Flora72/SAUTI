2907c5c684437ab20661bb6a7be5c9da560aeff3
Flora72 <zahratheo569@gmail.com>
2025-11-20 12:46:32 +0300

    Sign up and login routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Home from './pages/Home.jsx'
 import ShareStory from './pages/ShareStory.jsx'
 import About from './pages/About.jsx'
 import Contact from './pages/Contact.jsx'
+import SignUp from './pages/Signup.jsx'
+import Login from './pages/Login.jsx'
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Route path="/share" element={<ShareStory />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   )
 }
