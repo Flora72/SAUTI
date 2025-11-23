c0cc4649fe7f6260437d77c2494e639476ec08b7
Flora72 <zahratheo569@gmail.com>
2025-11-23 14:57:36 +0300

    added emergency page

---
+++ frontend/src/pages/Emergency.jsx
@@ -0,0 +1,8 @@
+export default function Emergency() {
+  return (
+    <div className="min-h-screen bg-base-200 text-base-content flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-600">Emergency Page Under Construction</h1>
+    </div>
+  )
+}
+
